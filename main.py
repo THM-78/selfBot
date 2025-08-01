972c15a9e1b018eff823920b1ecace0d1072e085
taha moradi <tahamoradi13851015@gmail.com>
2025-08-01 19:47:38 +0330

    some changes

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from telethon import TelegramClient, events
 from telethon.tl.types import Chat
 from config import client
 import handlers
-import install_ffmpeg
 async def main():
     print('Starting client...')
     await client.get_me()
--- utilities.py
+++ utilities.py
@@ -1,6 +1,8 @@
 from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
+import subprocess
 import os
-import ffmpeg
+import imageio_ffmpeg as ffmpeg
 
 # def convert_ogg_to_mp3(input_path):
 #     output_path = os.path.splitext(input_path)[0] + '.mp3'
@@ -14,19 +16,27 @@ def convert_to_mp3(input_path: str,output_format:str = "mp3" ,artist: str = None
     if output_format not in allowed_formats:
         print(f"⚠️ فرمت خروجی '{output_format}' پشتیبانی نمی‌شود. تبدیل به 'mp3' انجام خواهد شد.")
         output_format = 'mp3'
+
     output_path = os.path.splitext(input_path)[0] + f'.{output_format}'
 
-    # اجرای ffmpeg برای تبدیل فرمت
-    ffmpeg.input(input_path).output(output_path).run(quiet=True, overwrite_output=True)
+    ffmpeg_path = ffmpeg.get_ffmpeg_exe()
+
+    subprocess.run([
+        ffmpeg_path,
+        '-y',  # برای overwrite
+        '-i', input_path,
+        output_path
+    ], check=True)
+
     if artist and output_format == 'mp3':
         try:
             audio = EasyID3(output_path)
-
         except Exception:
-            from mutagen.mp3 import MP3
             audio = MP3(output_path, ID3=EasyID3)
             audio.add_tags()
             audio = EasyID3(output_path)
+
         audio['artist'] = artist
         audio.save()
+
     return output_path
